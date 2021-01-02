1d1fa1b631c075d2dd3068b1e7ca3cbcc6f073b7
Umbra <idk4090@gmail.com>
2021-01-02 15:48:00 +0200

    Add Animated Splash Screen

--- The Music of Des Rocs/The Music of Des Rocs.Android/SplashActivity.cs
+++ The Music of Des Rocs/The Music of Des Rocs.Android/SplashActivity.cs
@@ -1,6 +1,8 @@
 using Android.App;
 using Android.Content;
 using AndroidX.AppCompat.App;
+using System;
+using System.Threading.Tasks;
 
 namespace The_Music_of_Des_Rocs.Droid
 {
@@ -13,6 +15,20 @@ namespace The_Music_of_Des_Rocs.Droid
         protected override void OnResume()
         {
             base.OnResume();
+            // Display the json animation
+            SetContentView(Resource.Layout.splash);
+            // Initialize a Task
+            Task startupWork = new Task(() => { SimulateStartupAsync(); });
+            // Start the Task
+            startupWork.Start();
+        }
+
+        private async void SimulateStartupAsync()
+        {
+            // Wait for a while
+            await Task.Delay(1000);
+
+            // Start the App
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
